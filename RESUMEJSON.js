f6615250f054e93aabb1cff0f903a0be15c1fe59
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-09-22 11:28:01 +0530

    Create RESUMEJSON.js

---
+++ RESUMEJSON.js
@@ -0,0 +1,69 @@
+let resume={
+    "basics": {
+      "name": "Mano karan",
+      "label": "Full stack developer",
+      "image": "",
+      "email": "manoradha7@gmail.com",
+      "phone": "8838432335",
+      "summary": "A summary of Manokaran",
+      "location": {
+        "address": "108,marriyamman kovil st",
+        "postalCode": "621008",
+        "city": "Trichy",
+        "countryCode": "+91",
+        "region": "India"
+      },
+      "profiles": [{
+        "network": "GUVI",
+        "username": "manoradha7",
+        "url": "https://guvi.in/manoradha7"
+      }]
+    },
+    "education": [{
+      "institution": "University college of Engineering",
+      "area": "Computer Science",
+      "studyType": "Bachelor",
+      "startDate": "2017-09-04",
+      "endDate": "2021-07-21",
+      "score": "7.3",
+      "courses": [
+        "BE104 COMPUTER SCIENCE AND ENGINEERING"
+      ]
+    }],
+    "skills": [{
+      "name": "Web Development",
+      "level": "Bachelor",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "python"
+      ]
+    }],
+    "languages": [{
+      "language": "English,Tamil",
+      "fluency": "Moderate,Native Speaker"
+    }],
+    "interests": [{
+      "name": "Playing",
+      "keywords": [
+        "Foot ball",
+        "Cricket"
+      ]
+    }],
+    "projects": [{
+      "name": "Driver Drowsiness Dettection System using python openCV",
+      "description": "In this Python project, we will be using OpenCV for gathering the images from webcam and feed them into a Deep Learning model which will classify whether the person’s eyes are ‘Open’ or ‘Closed’. ",
+      "keywords": [
+        "Python"
+      ],
+      "startDate": "2020-12-15",
+      "endDate": "2021-07-22",
+      "roles": [
+        "Team Member"
+      ],
+      "entity": "Entry Level",
+      "type": "application"
+    }]
+  }
+  console.log(resume)
